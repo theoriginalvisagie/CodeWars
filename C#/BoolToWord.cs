a8607e49c3b38912efbc19b5269c873fb26240ee
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-12-02 08:55:49 +0200

    C# started
    BoolToWord - Returns Yes for true and No for False
    VowelCount - Counts # of Vowels in string

---
+++ C#/BoolToWord.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+/*
+* Return Yes for true and No for false.
+*/
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    if(word)
+     return "Yes";
+    else
+     return "No";
+  }
+}
---
+++ C#/VowelCount.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+/*
+* Counts amount of vowels in a string
+*/
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+      int vowelCount = 0;
+      for (int i = 0; i < str.Length; i++){
+        if (str[i]  == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
+                vowelCount++;
+        }
+      }
+      return vowelCount;
+    }
+}
