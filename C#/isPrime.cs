24374a7d003cd5dc58415961cbbddbfb3dfcbb30
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-17 10:04:32 +0200

    isPrime

---
+++ C#/isPrime.cs
@@ -0,0 +1,20 @@
+public static class Kata
+{
+    /*
+    * Checks if number is prime
+    */
+    public static bool IsPrime(int n)
+    {
+        if (n <= 1){
+            return false;
+        }       
+    
+        for (int i = 2; i*i <= n; i++){
+            if (n % i == 0){
+                return false;          
+            }
+        }
+
+        return true;
+    }
+}
